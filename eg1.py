67d24c1f21ca53399db784949c3afc9a27be1120
thuyatun <tunthura97@gmail.com>
2018-06-11 10:37:53 +0630

    flask python test

--- eg1.py
+++ eg1.py
@@ -1,1 +1,11 @@
-print("hello world")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "hello world"
+
+@app.route("/tiide")
+def tiide():
+    return "welcome to tiide world"
